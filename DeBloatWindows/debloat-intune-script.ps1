0644ee8647231e5be3d6aa94bde5f36c23aa544d
jamessingeraro <james.singer@aro.tech>
2024-03-19 16:53:22 +0000

    Edit

--- DeBloatWindows/debloat-intune-script.ps1
+++ DeBloatWindows/debloat-intune-script.ps1
@@ -12,7 +12,7 @@ Else {
 $templateFilePath = "C:\ProgramData\Debloat\removebloat.ps1"
 
 Invoke-WebRequest `
--Uri "https://github.com/jamessingeraro/BruhanIntuneFiles/blob/main/DeBloatWindows/RemoveBloat.ps1" `
+-Uri "https://raw.githubusercontent.com/jamessingeraro/BruhanIntuneFiles/main/DeBloatWindows/RemoveBloat.ps1" `
 -OutFile $templateFilePath `
 -UseBasicParsing `
 -Headers @{"Cache-Control"="no-cache"}
